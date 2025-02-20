5b732b0d0a58f20c87fdeb6f9bba711017c6985a
Santiago <santiago.ottolini98@gmail.com>
2025-02-20 06:54:19 -0300

    Dragon Ball - Add constants folder

--- src/components/character-card/character-card.component.jsx
+++ src/components/character-card/character-card.component.jsx
@@ -4,6 +4,8 @@ import React from 'react'
 import './character-card.styles.css'
 //Hooks
 import { useNavigate, useLocation } from 'react-router-dom'
+//Constants
+import constants from './constants/character-card.constants'
 
 function CharacterCard({
   name,
@@ -34,20 +36,11 @@ function CharacterCard({
             }}
           >
             <svg
-              width="20"
-              height="20"
-              viewBox="0 0 13 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+              {...constants.SVG_PROPS}
               className={`character-card__like ${isLiked ? 'liked' : ''}`}
             >
               <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="
-                  M6.5 2.43311L3.5 0.612305L0.5 2.43311V6.33485L6.5 11.4505L12.5 
-                  6.33485V2.43311L9.5 0.612305L6.5 2.43311Z
-                "
+                {...constants.SVG_PATH_PROPS}
                 fill={isLiked ? '#FFFFFF' : '#EC1D24'}
               />
             </svg>
